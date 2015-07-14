85dfef9c581f559220a9690eb0a29ce31e687e46
Dave Strus <dave@davestrus.com>
2015-07-14 17:47:18 -0400

    Add conditional validations for post link and body.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
+  enum post_type: [:link, :text]
   validates :title, length: { maximum: 160 }, presence: true
   validates :link, length: { maximum: 2000 }
-  enum post_type: [:link, :text]
+  validates :link, presence: true, if: :link?
+  validates :body, presence: true, if: :text?
 end
