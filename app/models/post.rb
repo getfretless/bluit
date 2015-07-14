92df36de095ab8405676019362a9cc221524a462
Dave Strus <dave@davestrus.com>
2015-07-14 16:32:15 -0400

    Validate presence of post title.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-  validates :title, length: { maximum: 160 }
+  validates :title, length: { maximum: 160 }, presence: true
   validates :link, length: { maximum: 2000 }
 end
