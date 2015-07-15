efbf64fa3e2c354211e51c661f89f3c3426c0edf
Dave Strus <dave@davestrus.com>
2015-07-14 20:45:12 -0400

    Extract flash messages into a helper method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,31 @@
 module ApplicationHelper
+
   def post_link(post)
     return post.link if post.link?
     return post_path(post) if post.text?
+  end
+
+  def flash_messages(flash)
+    output = ''
+    flash.each do |name, msg|
+      output += content_tag :div, class: "alert #{name}" do
+        if msg.is_a? Array
+          flash_list msg
+        else
+          msg
+        end
+      end
+    end
+    output.html_safe
+  end
+
+  def flash_list(messages)
+    content_tag :ul do
+      list_items = ''
+      messages.each do |message|
+        list_items += content_tag(:li, message)
+      end
+      list_items.html_safe
+    end
   end
 end
