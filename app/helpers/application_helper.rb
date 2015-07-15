a50b4de9c61caec9cef90ee43d29ea90259d8956
Dave Strus <dave@davestrus.com>
2015-07-15 09:55:47 -0400

    Refactor flash messages even more.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,26 +6,26 @@ module ApplicationHelper
   end
 
   def flash_messages(flash)
-    output = ''
-    flash.each do |name, msg|
-      output += content_tag :div, class: "alert #{name}" do
-        if msg.is_a? Array
-          flash_list msg
-        else
-          msg
-        end
+    flash.map do |name, msg|
+      content_tag :div, class: "alert #{name}" do
+        flash_message_output msg
       end
+    end.join.html_safe
+  end
+
+  def flash_message_output(content)
+    if content.is_a? Array
+      flash_list content
+    else
+      content
     end
-    output.html_safe
   end
 
   def flash_list(messages)
     content_tag :ul do
-      list_items = ''
-      messages.each do |message|
-        list_items += content_tag(:li, message)
-      end
-      list_items.html_safe
+      messages.map do |message|
+        content_tag(:li, message)
+      end.join.html_safe
     end
   end
 end
