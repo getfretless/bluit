db1fd25f04b060857499ce97a635502d70a55bd2
Dave Strus <dave@davestrus.com>
2015-07-15 16:34:53 -0400

    Assign a category to posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -50,6 +50,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :link, :body, :post_type)
+    params.require(:post).permit(:title, :link, :body, :post_type, :category_id)
   end
 end
