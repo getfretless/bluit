f22d7100588414402e68cee3abf97528d52d67ba
Dave Strus <dave@davestrus.com>
2015-07-16 10:44:27 -0400

    Create nested comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     post = Post.find params[:comment][:commentable_id]
     comment = Comment.build_from post, current_user.id, comment_params[:body]
@@ -11,9 +13,19 @@ class CommentsController < ApplicationController
     redirect_to post
   end
 
+  def new
+    @parent = Comment.find params[:parent_id]
+    post = @parent.commentable
+    @comment = Comment.build_from(post, current_user.id, '')
+    @comment.parent_id = @parent.id
+    respond_to do |format|
+      format.js {}
+    end
+  end
+
   private
 
   def comment_params
-    params.require(:comment).permit(:body, :commentable_name, :commentable_id, :commentable_type)
+    params.require(:comment).permit(:body, :commentable_name, :commentable_id, :commentable_type, :parent_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'posts#index'
   resources :posts
   resources :categories
-  resources :comments, only: :create
+  resources :comments, only: [:create, :new]
 end
