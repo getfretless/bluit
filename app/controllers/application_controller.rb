70ee8de02ba89d5a1b970c3dc702adc298835f72
Dave Strus <dave@davestrus.com>
2015-07-15 20:41:19 -0400

    Use Devise for authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :categories
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def categories
     @categories = Category.all if !defined?(@categories)
     @categories
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.for(:account_update) << :username
   end
 end
